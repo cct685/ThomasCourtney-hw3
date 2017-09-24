1905bb5244be981a09aee988a2335454adb999e7
cct685 <ccthomas530@icloud.com>
2017-09-24 11:07:34 -0500

    HW3

---
+++ ThomasCourtney-hw3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ThomasCourtney-hw3
+//
+//  Created by Courtney Thomas on 9/24/17.
+//  Copyright © 2017 CS329E. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
